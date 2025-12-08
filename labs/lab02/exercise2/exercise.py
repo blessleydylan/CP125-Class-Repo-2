de19f122ed5f787c8bc31c4043b77926c85c2454
blessleydylan <blessleydylanworkmail@proton.me>
2025-12-08 11:48:37 +0800

    Complete Lab 2 Exercise 2

--- labs/lab02/exercise2/exercise.py
+++ labs/lab02/exercise2/exercise.py
@@ -6,7 +6,11 @@ import math
 def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
     # TODO: Implement this function
     # Calculate total cost for tents and meals
-    pass
+    no_tents = participants / tent_capacity
+    total_tent_price = no_tents * tent_price
+    total_meal_price = meal_price * participants
+    total_price = total_tent_price + total_meal_price
+    return total_price
 
 # Test your code here
 print("Testing Camping Logistics...")
