dbb727d6814988ff6ef0a2476691a0c28b4071bb
blessleydylan <blessleydylanworkmail@proton.me>
2025-12-08 11:43:44 +0800

    Correct Lab 2 Exercise 1

--- labs/lab02/exercise1/exercise.py
+++ labs/lab02/exercise1/exercise.py
@@ -4,15 +4,19 @@
 def is_budget_sufficient(one_way_km, km_per_liter, price_per_liter, budget):
     # TODO: Implement this function
     # Calculate round trip cost and checks if within budget
-    total_distance = one_way_km * km_per_liter
-    needed_budget = total_distance / price_per_liter
-    round_trip_budget = needed_budget * 2
+    total_distance = (one_way_km * 2)
+    total_liter = total_distance / km_per_liter
+    needed_budget = total_liter * price_per_liter
     
     # Check if budget is enough for a round trip
-    if round_trip_budget < budget:
+    if needed_budget <= budget:
         round_trip = True
+        return round_trip
+    else: 
+        round_trip = False
         return round_trip
 
 
 # Test your code here
 print("Testing Road Trip Budgeter...")
+print(is_budget_sufficient(100, 10, 2.00, 40))
