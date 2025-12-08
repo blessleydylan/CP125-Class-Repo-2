4041fc1da7e70023c7969994309efa9e9dea2227
blessleydylan <blessleydylanworkmail@proton.me>
2025-12-08 12:08:07 +0800

    Correct Lab 2 Exercise 2 and Exercise 3

--- labs/lab02/exercise2/exercise2.py
+++ labs/lab02/exercise2/exercise2.py
@@ -6,7 +6,7 @@ import math
 def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
     # TODO: Implement this function
     # Calculate total cost for tents and meals
-    no_tents = participants / tent_capacity
+    no_tents = math.ceil(participants / tent_capacity)
     total_tent_price = no_tents * tent_price
     total_meal_price = meal_price * participants
     total_price = total_tent_price + total_meal_price
---
+++ labs/lab02/exercise4/exercise.py
@@ -0,0 +1,14 @@
+# Lab 02 Exercise 4: Dynamic Parking Rate
+# Write your code below:
+
+def get_hourly_rate(vehicle_type, hour_24):
+    # TODO: Implement this function
+    # Return hourly rate based on vehicle and time
+    if (vehicle_type == "Electric"):
+        total_price = 2.00
+    elif (vehicle_type == "Hybrid") and ((hour_24 < 6) or (hour_24 > 22)):
+        total_price = 2.00
+    elif (vehicle_type == "Hybrid") and ((hour_24 >= 6) or (hour_24 < 22)):
+        
+# Test your code here
+print("Testing Dynamic Parking Rate...")
