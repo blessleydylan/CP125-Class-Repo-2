c4fccfade95182b483dfa9a4c85c824016c690f6
blessleydylan <blessleydylanworkmail@proton.me>
2025-12-15 11:34:28 +0800

    Correct Lab 02 Exercise 5

--- labs/lab02/exercise5/exercise5.py
+++ labs/lab02/exercise5/exercise5.py
@@ -1,5 +1,5 @@
 def is_valid_triangle(a ,b ,c):
-    if ((a + b) > c) or ((a + c) > b) or ((b + c) > a):
+    if ((a + b) > c) and ((a + c) > b) and ((b + c) > a):
         return True
     else:
         return False
