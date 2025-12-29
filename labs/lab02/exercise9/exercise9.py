0aa3a2a8c4a44313d8e054d8b38e68b48bea6b39
blessleydylan <blessleydylanworkmail@proton.me>
2025-12-29 12:04:31 +0800

    Complete Lab 2 Exercise 9

--- labs/lab02/exercise9/exercise9.py
+++ labs/lab02/exercise9/exercise9.py
@@ -3,7 +3,8 @@ def calculate_xp_required(current_level):
     Calculate XP needed for next level (level * 100).
     """
     # TODO: Implement this
-    pass
+    xp_required = current_level * 100
+    return xp_required
 
 
 def can_level_up(current_xp, required_xp):
@@ -11,7 +12,10 @@ def can_level_up(current_xp, required_xp):
     Check if player has enough XP to level up.
     """
     # TODO: Implement this
-    pass
+    if current_xp >= required_xp:
+        return True
+    else:
+        return False
 
 
 def calculate_final_level(total_xp):
@@ -19,12 +23,29 @@ def calculate_final_level(total_xp):
     Calculate the final level reached.
     """
     # TODO: Implement this
-    pass
-
+    required_xp = 100
+    level = 1
+    while True:
+        if total_xp >= required_xp:
+            level += 1
+            total_xp -= required_xp
+            required_xp += 100
+        else:
+            break
+    return level
 
 def calculate_remaining_xp(total_xp):
     """
     Calculate XP leftover after leveling.
     """
     # TODO: Implement this
-    pass
+    required_xp = 100
+    level = 1
+    while True:
+        if total_xp >= required_xp:
+            level += 1
+            total_xp -= required_xp
+            required_xp += 100
+        else:
+            break
+    return total_xp
