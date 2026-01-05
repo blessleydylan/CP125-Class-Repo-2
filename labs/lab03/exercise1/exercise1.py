34977a8692424c7645400cafbc454d50e49a87be
blessleydylan <blessleydylanworkmail@proton.me>
2026-01-05 11:49:42 +0800

    Compelte Lab 3 Exercise 1

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,12 @@
 def count_bright_spots(pixels):
-    pass
+    bright_spots = 0
+
+    # A bright spot needs two neighbors
+    if len(pixels) < 3:
+        return 0
+
+    for item in range(1, len(pixels) - 1):
+        if pixels[item - 1] < pixels[item] > pixels[item + 1]:
+            bright_spots += 1
+
+    return bright_spots
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,5 @@
 def find_station(stations, name):
-    pass
+    
 
 def count_stops(stations, start, stop):
     pass
