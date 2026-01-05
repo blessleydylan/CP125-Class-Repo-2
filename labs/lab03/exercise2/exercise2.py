19617914845e8ecbe28babdabd3704a0f7fb3020
blessleydylan <blessleydylanworkmail@proton.me>
2026-01-05 13:07:06 +0800

    Complete Lab 03 Exercises

--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,34 @@
 def find_station(stations, name):
-    
+    station_number = 0
+    for item in stations:
+        if item == name:
+            return station_number
+        station_number += 1
+    return None
+
 
 def count_stops(stations, start, stop):
-    pass
+    station_number = 0
+    station_number2 = 0
+
+    # find start position
+    for item in stations:
+        if item == start:
+            break
+        station_number += 1
+    else:
+        return -1   # start not found
+
+    # find stop position
+    for item2 in stations:
+        if item2 == stop:
+            break
+        station_number2 += 1
+    else:
+        return -1   # stop not found
+
+    # calculate stops
+    if station_number > station_number2:
+        return station_number - station_number2
+    else:
+        return station_number2 - station_number
--- labs/lab03/exercise5/exercise5.py
+++ labs/lab03/exercise5/exercise5.py
@@ -1,5 +1,23 @@
 def get_position(cars, car_number):
-    pass
+    for item in range(len(cars)):
+        if cars[item] == car_number:
+            return item
 
 def has_overtaken(before, after, car1, car2):
-    pass
+    for i in range(len(before)):
+        if before[i] == car1:
+            car1_before = i
+        if before[i] == car2:
+            car2_before = i
+
+    for j in range(len(after)):
+        if after[j] == car1:
+            car1_after = j
+        if after[j] == car2:
+            car2_after = j
+
+    if car1_before > car2_before and car1_after < car2_after:
+        return True
+    else:
+        return False
+
