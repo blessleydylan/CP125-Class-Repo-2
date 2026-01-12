df8c55cc94391168eb586bb34c5a6f5e2bc015bc
blessleydylan <blessleydylanworkmail@proton.me>
2026-01-12 12:32:28 +0800

    Complete Lab 4 Exercise 4

--- labs/lab04/exercise4/exercise4.py
+++ labs/lab04/exercise4/exercise4.py
@@ -1,7 +1,18 @@
-
 def analyze_performance(lap_times):
-  
-    pass
+    first_half = []
+    second_half = []
+    
+    total_laps = len(lap_times)
+    midpoint = (total_laps + 1) // 2  
+    
+    for i in range(total_laps):
+        if i < midpoint:
+            first_half.append(lap_times[i])
+        else:
+            second_half.append(lap_times[i])
+    first_avg = sum(first_half) / len(first_half)
+    second_avg = sum(second_half) / len(second_half)
+    return second_avg > first_avg
 
 
 # Test
