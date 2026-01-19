946cd1fd1486d67a7e1d3283b5ebc2f295bcc36b
blessleydylan <blessleydylanworkmail@proton.me>
2026-01-19 12:25:37 +0800

    Complete Lab 05 Exercise 4

--- labs/lab05/exercise4/exercise4.py
+++ labs/lab05/exercise4/exercise4.py
@@ -1,5 +1,18 @@
 def filter_query_times(query_times):
-    a
+    if len(query_times) == 0:
+        return []
+
+    sorted_times = sorted(query_times)
+    n = len(sorted_times)
+    
+    mean = sum(sorted_times) / n
+    variance = (sum(x * x for x in sorted_times) / n) - (mean ** 2)
+    std_dev = variance ** 0.5
+    
+    upper_limit = mean + std_dev
+
+    return [x for x in sorted_times if x <= upper_limit]
+
 
 
 # Test
