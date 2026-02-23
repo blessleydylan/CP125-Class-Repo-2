2ebb3772b77f3a195368b95abe30e94909151fe0
blessleydylan <blessleydylanworkmail@proton.me>
2026-02-23 12:12:17 +0800

    Fixed Coding

--- labs/lab06/exercise2/exercise2.py
+++ labs/lab06/exercise2/exercise2.py
@@ -1,11 +1,25 @@
 def match_specialists(candidates_list, project_requirements):
-    count_map = {}
+    skill_frequency = {}
 
-    for skill in project_requirements:
-        if skill in count_map:
-            count_map[skill] += 1
-        else:
-            count_map[skill] = 1
-    print(count_map)
+    for name, skills in candidates_list:
+        for skill in skills:
+            if skill not in skill_frequency:
+                skill_frequency[skill] = 0
+            skill_frequency[skill] += 1
 
-match_specialists(["Ali", {"Python", "Git"}],)
+    rare_skills_set = set()
+
+    for skill, count in skill_frequency.items():
+        if count < 3:
+            rare_skills_set.add(skill)
+
+    specialists = []
+
+    for name, skills in candidates_list:
+            if project_requirements.issubset(skills):
+                 candidate_rare_skills = skills & rare_skills_set
+
+                 if len(candidate_rare_skills) > 0:
+                      specialists.append((name, candidate_rare_skills))
+
+    return sorted(specialists)
