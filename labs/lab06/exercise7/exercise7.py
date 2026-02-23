e6360e0dd6fae9ef25fc9d23dfcb94e4d037d7b6
blessleydylan <blessleydylanworkmail@proton.me>
2026-02-23 11:34:44 +0800

    Compelte lab 06 Exercise 07

--- labs/lab06/exercise7/exercise7.py
+++ labs/lab06/exercise7/exercise7.py
@@ -1,14 +1,12 @@
 def manage_playlist(current_playlist, add_songs, import_playlist, banned_songs):
-    """
-    Manages a music playlist with adds, imports, and removals.
+    for song in add_songs:
+        current_playlist.add(song)
+
+    current_playlist.update(import_playlist)
+    current_playlist -= banned_songs
+
+    while len(current_playlist) > 6:
+        current_playlist.pop()
+
+    return len(current_playlist)
     
-    Args:
-        current_playlist: Set of currently in playlist
-        add_songs: List of songs to add individually
-        import_playlist: Set of songs to import from Spotify
-        banned_songs: Set of songs to remove
-    
-    Returns:
-        int: Count of final songs in playlist
-    """
-    pass
