2bf1e3e62d05a5b2fbab8ff621ef17d40ec28f83
blessleydylan <blessleydylanworkmail@proton.me>
2026-02-23 11:41:50 +0800

    Complete Lab 06 Exercise 09

--- labs/lab06/exercise9/exercise9.py
+++ labs/lab06/exercise9/exercise9.py
@@ -18,9 +18,7 @@ def find_qualified_students(student_records, required_courses):
     qualified = []
     
     for student_id, completed in student_records:
-        common = completed | required_courses
-        
-        if common == completed:
+        if completed >= required_courses:
             qualified.append(student_id)
     
-    return qualified
+    return sorted(qualified)
