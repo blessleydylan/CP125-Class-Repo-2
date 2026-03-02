280d87ed0f8408deef7a2c254f9fc2955b80f30e
blessleydylan <blessleydylanworkmail@proton.me>
2026-03-02 13:10:01 +0800

    Save Directories

--- labs/lab08/exercise3/exercise3.py
+++ labs/lab08/exercise3/exercise3.py
@@ -1,7 +1,10 @@
 # Lab 08 Exercise 3: Product Price Lookup
 # Write your code below:
+import csv
 
 def calculate_order_total(products_file, order_file, output_file):
+
+    
     """
     Calculate total cost for each product in order.
 
@@ -18,5 +21,7 @@ def calculate_order_total(products_file, order_file, output_file):
 
 
 # Test your code here
-result = calculate_order_total("data/products.csv", "data/order.csv", "data/total.csv")
+result = calculate_order_total("CP125-Class-Repo-2/labs/lab08/exercise3/data/products.csv",
+                                "CP125-Class-Repo-2/labs/lab08/exercise3/data/order.csv",
+                                "CP125-Class-Repo-2/labs/lab08/exercise3/data/total.csv")
 print(f"Grand total: ${result:.2f}")
