5b19ee69a2b22eabcf1b88861c6a931abb8e88e6
blessleydylan <blessleydylanworkmail@proton.me>
2026-04-02 10:04:40 +0800

    Halfly finish Lab 09

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -1,5 +1,24 @@
 import pandas as pd
 
-
 def explore_data(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    # Total number of students (rows)
+    total_students = df.shape[0]
+
+    # Subjects list (fixed order as required by tests)
+    subjects = ["Math", "Science", "English"]
+
+    # Math average
+    math_average = df["Math"].mean()
+
+    # Student with highest Math score
+    idx = df["Math"].idxmax()
+    highest_math_student = df.loc[idx, "Name"]
+
+    return {
+        "total_students": total_students,
+        "subjects": subjects,
+        "math_average": math_average,
+        "highest_math_student": highest_math_student
+    }
